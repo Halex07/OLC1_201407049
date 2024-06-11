b2863c97ff5b8bc89e8878140887da7eebaaf260
Halex07 <92344208+Halex07@users.noreply.github.com>
2024-06-10 20:55:02 -0600

    Add files via upload

---
+++ Proyecto 1_JavaCraft/src/simbolo/tipoDato.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simbolo;
+
+/**
+ *
+ * @author Alex
+ */
+public class tipoDato {
+    ENTERO,
+    DECIMAL,
+    BOOLEANO,
+    CARACTER,
+    CADENA,
+    VOID
+    
+}
